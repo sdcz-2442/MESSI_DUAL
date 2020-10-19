a0a7ecbe092f035fb13ce9f64b529598c1fcc789
sdcz-2442 <71774412+sdcz-2442@users.noreply.github.com>
2020-10-19 18:15:46 +0200

    PRUEBA

---
+++ MESSI_FormBase/frm_base.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MESSI_FormBase
+{
+    public partial class frm_base : Form
+    {
+        public frm_base()
+        {
+            InitializeComponent();
+        }
+
+        private void btn_closewindow_Click(object sender, EventArgs e)
+        {
+            //Sortir
+            this.Close();
+        }
+
+        private void btn_minimize_Click(object sender, EventArgs e)
+        {
+            //Minimitzar
+            //this.WindowState = FormWindowState.Minimized();
+        }
+
+        private void frm_base_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btn_closewindow_Click_1(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void btn_minimizewindow_Click(object sender, EventArgs e)
+        {
+            this.WindowState = FormWindowState.Minimized;
+        }
+    }
+}
---
+++ MESSI_formmenu/frm_basemenu.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MESSI_formmenu
+{
+    public partial class frm_basemenu: MESSI_FormBase.frm_base
+    {
+        public frm_basemenu()
+        {
+            InitializeComponent();
+        }
+
+        private void UserControl1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}

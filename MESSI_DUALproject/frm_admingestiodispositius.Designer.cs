8f8a8ce393257a694a6c57641b87e76b995f0b36
sdcz-2442 <71774412+sdcz-2442@users.noreply.github.com>
2020-11-23 19:52:41 +0100

    Gestion de dispositivos

--- MESSI_DUALproject/frm_admingestiodispositius.Designer.cs
+++ MESSI_DUALproject/frm_admingestiodispositius.Designer.cs
@@ -30,8 +30,8 @@
         {
             this.panel3 = new System.Windows.Forms.Panel();
             this.lbl_nom = new System.Windows.Forms.Label();
-            this.btn_generarcoord = new System.Windows.Forms.Button();
-            this.button1 = new System.Windows.Forms.Button();
+            this.btn_guardardisp = new System.Windows.Forms.Button();
+            this.btn_delete_coord = new System.Windows.Forms.Button();
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
             this.panel2 = new System.Windows.Forms.Panel();
@@ -44,10 +44,9 @@
             // 
             this.panel3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(67)))), ((int)(((byte)(111)))), ((int)(((byte)(138)))));
             this.panel3.Controls.Add(this.lbl_nom);
-            this.panel3.Location = new System.Drawing.Point(0, 26);
-            this.panel3.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.panel3.Location = new System.Drawing.Point(0, 21);
             this.panel3.Name = "panel3";
-            this.panel3.Size = new System.Drawing.Size(457, 68);
+            this.panel3.Size = new System.Drawing.Size(343, 55);
             this.panel3.TabIndex = 8;
             // 
             // lbl_nom
@@ -55,62 +54,58 @@
             this.lbl_nom.AutoSize = true;
             this.lbl_nom.Font = new System.Drawing.Font("Gill Sans Nova", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.lbl_nom.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(247)))), ((int)(((byte)(251)))), ((int)(((byte)(225)))));
-            this.lbl_nom.Location = new System.Drawing.Point(45, 14);
-            this.lbl_nom.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lbl_nom.Location = new System.Drawing.Point(34, 11);
             this.lbl_nom.Name = "lbl_nom";
-            this.lbl_nom.Size = new System.Drawing.Size(276, 44);
+            this.lbl_nom.Size = new System.Drawing.Size(217, 35);
             this.lbl_nom.TabIndex = 5;
             this.lbl_nom.Text = "Trusted devices";
             // 
-            // btn_generarcoord
+            // btn_guardardisp
             // 
-            this.btn_generarcoord.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(67)))), ((int)(((byte)(111)))), ((int)(((byte)(138)))));
-            this.btn_generarcoord.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(67)))), ((int)(((byte)(111)))), ((int)(((byte)(138)))));
-            this.btn_generarcoord.FlatAppearance.MouseDownBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(67)))), ((int)(((byte)(138)))), ((int)(((byte)(94)))));
-            this.btn_generarcoord.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(67)))), ((int)(((byte)(138)))), ((int)(((byte)(94)))));
-            this.btn_generarcoord.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.btn_generarcoord.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btn_generarcoord.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(247)))), ((int)(((byte)(251)))), ((int)(((byte)(225)))));
-            this.btn_generarcoord.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
-            this.btn_generarcoord.Location = new System.Drawing.Point(148, 369);
-            this.btn_generarcoord.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
-            this.btn_generarcoord.Name = "btn_generarcoord";
-            this.btn_generarcoord.Padding = new System.Windows.Forms.Padding(11, 0, 0, 0);
-            this.btn_generarcoord.Size = new System.Drawing.Size(167, 44);
-            this.btn_generarcoord.TabIndex = 9;
-            this.btn_generarcoord.Text = "Save";
-            this.btn_generarcoord.UseVisualStyleBackColor = false;
-            this.btn_generarcoord.Click += new System.EventHandler(this.btn_generarcoord_Click);
+            this.btn_guardardisp.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(67)))), ((int)(((byte)(111)))), ((int)(((byte)(138)))));
+            this.btn_guardardisp.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(67)))), ((int)(((byte)(111)))), ((int)(((byte)(138)))));
+            this.btn_guardardisp.FlatAppearance.MouseDownBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(67)))), ((int)(((byte)(138)))), ((int)(((byte)(94)))));
+            this.btn_guardardisp.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(67)))), ((int)(((byte)(138)))), ((int)(((byte)(94)))));
+            this.btn_guardardisp.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn_guardardisp.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_guardardisp.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(247)))), ((int)(((byte)(251)))), ((int)(((byte)(225)))));
+            this.btn_guardardisp.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btn_guardardisp.Location = new System.Drawing.Point(111, 300);
+            this.btn_guardardisp.Name = "btn_guardardisp";
+            this.btn_guardardisp.Padding = new System.Windows.Forms.Padding(8, 0, 0, 0);
+            this.btn_guardardisp.Size = new System.Drawing.Size(125, 36);
+            this.btn_guardardisp.TabIndex = 9;
+            this.btn_guardardisp.Text = "Save";
+            this.btn_guardardisp.UseVisualStyleBackColor = false;
+            this.btn_guardardisp.Click += new System.EventHandler(this.btn_guardardisp_Click);
             // 
-            // button1
+            // btn_delete_coord
             // 
-            this.button1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(67)))), ((int)(((byte)(111)))), ((int)(((byte)(138)))));
-            this.button1.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(67)))), ((int)(((byte)(111)))), ((int)(((byte)(138)))));
-            this.button1.FlatAppearance.MouseDownBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(67)))), ((int)(((byte)(138)))), ((int)(((byte)(94)))));
-            this.button1.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(67)))), ((int)(((byte)(138)))), ((int)(((byte)(94)))));
-            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(247)))), ((int)(((byte)(251)))), ((int)(((byte)(225)))));
-            this.button1.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
-            this.button1.Location = new System.Drawing.Point(373, 369);
-            this.button1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
-            this.button1.Name = "button1";
-            this.button1.Padding = new System.Windows.Forms.Padding(11, 0, 0, 0);
-            this.button1.Size = new System.Drawing.Size(167, 44);
-            this.button1.TabIndex = 10;
-            this.button1.Text = "Delete";
-            this.button1.UseVisualStyleBackColor = false;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
+            this.btn_delete_coord.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(67)))), ((int)(((byte)(111)))), ((int)(((byte)(138)))));
+            this.btn_delete_coord.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(67)))), ((int)(((byte)(111)))), ((int)(((byte)(138)))));
+            this.btn_delete_coord.FlatAppearance.MouseDownBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(67)))), ((int)(((byte)(138)))), ((int)(((byte)(94)))));
+            this.btn_delete_coord.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(67)))), ((int)(((byte)(138)))), ((int)(((byte)(94)))));
+            this.btn_delete_coord.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn_delete_coord.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_delete_coord.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(247)))), ((int)(((byte)(251)))), ((int)(((byte)(225)))));
+            this.btn_delete_coord.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btn_delete_coord.Location = new System.Drawing.Point(280, 300);
+            this.btn_delete_coord.Name = "btn_delete_coord";
+            this.btn_delete_coord.Padding = new System.Windows.Forms.Padding(8, 0, 0, 0);
+            this.btn_delete_coord.Size = new System.Drawing.Size(125, 36);
+            this.btn_delete_coord.TabIndex = 10;
+            this.btn_delete_coord.Text = "Delete";
+            this.btn_delete_coord.UseVisualStyleBackColor = false;
+            this.btn_delete_coord.Click += new System.EventHandler(this.btn_delete_coord_Click);
             // 
             // label1
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Gill Sans Nova", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(67)))), ((int)(((byte)(111)))), ((int)(((byte)(138)))));
-            this.label1.Location = new System.Drawing.Point(140, 191);
-            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Location = new System.Drawing.Point(105, 155);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(110, 44);
+            this.label1.Size = new System.Drawing.Size(87, 35);
             this.label1.TabIndex = 6;
             this.label1.Text = "MAC:";
             // 
@@ -119,10 +114,9 @@
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Gill Sans Nova", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(67)))), ((int)(((byte)(111)))), ((int)(((byte)(138)))));
-            this.label2.Location = new System.Drawing.Point(140, 263);
-            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Location = new System.Drawing.Point(105, 214);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(205, 44);
+            this.label2.Size = new System.Drawing.Size(162, 35);
             this.label2.TabIndex = 11;
             this.label2.Text = "HostName:";
             // 
@@ -130,54 +124,51 @@
             // 
             this.panel2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(67)))), ((int)(((byte)(111)))), ((int)(((byte)(138)))));
             this.panel2.Dock = System.Windows.Forms.DockStyle.Bottom;
-            this.panel2.Location = new System.Drawing.Point(0, 533);
-            this.panel2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.panel2.Location = new System.Drawing.Point(0, 433);
             this.panel2.Name = "panel2";
-            this.panel2.Size = new System.Drawing.Size(1067, 21);
+            this.panel2.Size = new System.Drawing.Size(800, 17);
             this.panel2.TabIndex = 12;
             // 
             // tbx_usermac
             // 
             this.tbx_usermac.Font = new System.Drawing.Font("Gill Sans Nova", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.tbx_usermac.Location = new System.Drawing.Point(373, 191);
-            this.tbx_usermac.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.tbx_usermac.Location = new System.Drawing.Point(280, 155);
             this.tbx_usermac.Multiline = true;
             this.tbx_usermac.Name = "tbx_usermac";
             this.tbx_usermac.ReadOnly = true;
-            this.tbx_usermac.Size = new System.Drawing.Size(264, 42);
+            this.tbx_usermac.Size = new System.Drawing.Size(199, 35);
             this.tbx_usermac.TabIndex = 13;
             // 
             // tbx_userhostname
             // 
             this.tbx_userhostname.Font = new System.Drawing.Font("Gill Sans Nova", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.tbx_userhostname.Location = new System.Drawing.Point(373, 263);
-            this.tbx_userhostname.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.tbx_userhostname.Location = new System.Drawing.Point(280, 214);
             this.tbx_userhostname.Multiline = true;
             this.tbx_userhostname.Name = "tbx_userhostname";
             this.tbx_userhostname.ReadOnly = true;
-            this.tbx_userhostname.Size = new System.Drawing.Size(379, 42);
+            this.tbx_userhostname.Size = new System.Drawing.Size(285, 35);
             this.tbx_userhostname.TabIndex = 14;
             // 
             // frm_admingestiodispositius
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1067, 554);
+            this.ClientSize = new System.Drawing.Size(800, 450);
             this.Controls.Add(this.tbx_userhostname);
             this.Controls.Add(this.tbx_usermac);
             this.Controls.Add(this.panel2);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
-            this.Controls.Add(this.button1);
-            this.Controls.Add(this.btn_generarcoord);
+            this.Controls.Add(this.btn_delete_coord);
+            this.Controls.Add(this.btn_guardardisp);
             this.Controls.Add(this.panel3);
-            this.Margin = new System.Windows.Forms.Padding(5, 5, 5, 5);
+            this.Margin = new System.Windows.Forms.Padding(4);
             this.Name = "frm_admingestiodispositius";
             this.Text = "frm_admingestiodispositius";
             this.Load += new System.EventHandler(this.frm_admingestiodispositius_Load);
             this.Controls.SetChildIndex(this.panel3, 0);
-            this.Controls.SetChildIndex(this.btn_generarcoord, 0);
-            this.Controls.SetChildIndex(this.button1, 0);
+            this.Controls.SetChildIndex(this.btn_guardardisp, 0);
+            this.Controls.SetChildIndex(this.btn_delete_coord, 0);
             this.Controls.SetChildIndex(this.label1, 0);
             this.Controls.SetChildIndex(this.label2, 0);
             this.Controls.SetChildIndex(this.panel2, 0);
@@ -194,8 +185,8 @@
 
         private System.Windows.Forms.Panel panel3;
         private System.Windows.Forms.Label lbl_nom;
-        private System.Windows.Forms.Button btn_generarcoord;
-        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button btn_guardardisp;
+        private System.Windows.Forms.Button btn_delete_coord;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Panel panel2;
--- MESSI_DUALproject/frm_admingestiodispositius.cs
+++ MESSI_DUALproject/frm_admingestiodispositius.cs
@@ -7,12 +7,14 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Data.SqlClient;
+using System.Collections;
 
 namespace MESSI_DUAL
 {
     public partial class frm_admingestiodispositius : MESSI_FormBase.frm_base
     {
-
+        bool existe;
         public frm_admingestiodispositius()
         {
             InitializeComponent();
@@ -20,13 +22,14 @@ namespace MESSI_DUAL
 
         public void frm_admingestiodispositius_Load(object sender, EventArgs e)
         {
-            bool existe;
+          
             var macAddr =
             (
                 from nic in System.Net.NetworkInformation.NetworkInterface.GetAllNetworkInterfaces()
                 where nic.OperationalStatus == System.Net.NetworkInformation.OperationalStatus.Up
                 select nic.GetPhysicalAddress().ToString()
             ).FirstOrDefault();
+
 
             string MACformateada = "";
             for (int i = 0; i < macAddr.Length; i++)
@@ -41,28 +44,107 @@ namespace MESSI_DUAL
             tbx_userhostname.Text = System.Environment.MachineName;
         }
 
-        private void btn_generarcoord_Click(object sender, EventArgs e)
+        private void btn_guardardisp_Click(object sender, EventArgs e)
         {
+            var macAddr =
+            (
+                from nic in System.Net.NetworkInformation.NetworkInterface.GetAllNetworkInterfaces()
+                where nic.OperationalStatus == System.Net.NetworkInformation.OperationalStatus.Up
+                select nic.GetPhysicalAddress().ToString()
+            ).FirstOrDefault();
+            String HostName = System.Environment.MachineName;
 
-            if (existe)
+            SqlConnection conn;
+            string cnx;
+            cnx = "Data Source=LAPTOP-45H9O8I4\\SQLEXPRESS;Initial Catalog=DarkCore;Integrated Security=True";
+            conn = new SqlConnection(cnx);
+
+            DataSet dts = new DataSet();
+            String query = "Select * from TrustedDevices where MAC = '" + macAddr + "'";
+
+            SqlDataAdapter adapter1;
+            adapter1 = new SqlDataAdapter(query, conn);
+
+            conn.Open();
+            adapter1.Fill(dts);
+            conn.Close();
+
+            if (dts.Tables[0].Rows.Count == 0)
             {
-                
+                //GUARDA REGISTRO
+                query = "INSERT INTO TrustedDevices(MAC,HostName) VALUES ('" + macAddr + "','" + HostName + "')";
+
+                SqlDataAdapter adapter2;
+                adapter2 = new SqlDataAdapter(query, conn);
+
+                conn.Open();
+                adapter2.Fill(dts);
+
+                if (dts.HasChanges())
+                {
+                    int result = adapter2.Update(dts.Tables[0]);
+                }
+
+                conn.Close();
+
+                MessageBox.Show("Registro guardado");
+
             }
             else
             {
-                MessageBox.Show("Error");
+                //NO USAR BOTÓN
+                MessageBox.Show("El dispositivo ya existe");
             }
         }
 
-        private void button1_Click(object sender, EventArgs e)
+        private void btn_delete_coord_Click(object sender, EventArgs e)
         {
-            if (existe)
+            var macAddr =
+            (
+                from nic in System.Net.NetworkInformation.NetworkInterface.GetAllNetworkInterfaces()
+                where nic.OperationalStatus == System.Net.NetworkInformation.OperationalStatus.Up
+                select nic.GetPhysicalAddress().ToString()
+            ).FirstOrDefault();
+
+            SqlConnection conn;
+            string cnx;
+            cnx = "Data Source=LAPTOP-45H9O8I4\\SQLEXPRESS;Initial Catalog=DarkCore;Integrated Security=True";
+            conn = new SqlConnection(cnx);
+
+            DataSet dts = new DataSet();
+            String query = "Select * from TrustedDevices where MAC = '" + macAddr + "'";
+
+            SqlDataAdapter adapter1;
+            adapter1 = new SqlDataAdapter(query, conn);
+
+            conn.Open();
+            adapter1.Fill(dts);
+            conn.Close();
+
+            if (dts.Tables[0].Rows.Count == 0)
             {
-                tbx_usermac.Clear();
+                //NO DEJAR UTILIZAR EL BOTÓN PORQUE LA MAC NO EXISTE EN LA BASE DE DATOS
+                MessageBox.Show("No se puede borrar dispositivo porque no existe");
             }
             else
             {
-                MessageBox.Show("Error");
+                //BORRAR EL REGISTRO DE LA BASE DE DATOS
+                query = "DELETE FROM TrustedDevices where MAC = '" + macAddr + "'";
+
+                SqlDataAdapter adapter2;
+                adapter2 = new SqlDataAdapter(query, conn);
+
+                conn.Open();
+                adapter2.Fill(dts);
+
+                if (dts.HasChanges())
+                {
+                    int result = adapter2.Update(dts.Tables[0]);
+                }
+
+                conn.Close();
+
+                MessageBox.Show("Registro borrado");
             }
         }
     }

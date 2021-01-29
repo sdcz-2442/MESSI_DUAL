9a5a8f63cb6a2cba8b3015dcbaea046d97461476
sdcz-2442 <71774412+sdcz-2442@users.noreply.github.com>
2021-01-29 13:21:27 +0100

    frm_balisses frontend editado

--- MESSI_DUALproject/frm_balisses.Designer.cs
+++ MESSI_DUALproject/frm_balisses.Designer.cs
@@ -30,13 +30,13 @@ namespace App
         private void InitializeComponent()
         {
             this.btn_connectar = new System.Windows.Forms.Button();
-            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.cmb_arduinoport = new System.Windows.Forms.ComboBox();
             this.panel1 = new System.Windows.Forms.Panel();
-            this.btn_configurar = new System.Windows.Forms.Button();
-            this.textBox1 = new System.Windows.Forms.TextBox();
-            this.textBox2 = new System.Windows.Forms.TextBox();
-            this.lbl_login = new System.Windows.Forms.Label();
             this.label1 = new System.Windows.Forms.Label();
+            this.lbl_login = new System.Windows.Forms.Label();
+            this.tbx_divisor = new System.Windows.Forms.TextBox();
+            this.tbx_grupscicle = new System.Windows.Forms.TextBox();
+            this.btn_configurar = new System.Windows.Forms.Button();
             this.panel1.SuspendLayout();
             this.SuspendLayout();
             // 
@@ -54,25 +54,63 @@ namespace App
             this.btn_connectar.UseVisualStyleBackColor = false;
             this.btn_connectar.Click += new System.EventHandler(this.btn_connectar_Click);
             // 
-            // comboBox1
+            // cmb_arduinoport
             // 
-            this.comboBox1.FormattingEnabled = true;
-            this.comboBox1.Location = new System.Drawing.Point(78, 71);
-            this.comboBox1.Name = "comboBox1";
-            this.comboBox1.Size = new System.Drawing.Size(248, 21);
-            this.comboBox1.TabIndex = 10;
+            this.cmb_arduinoport.FormattingEnabled = true;
+            this.cmb_arduinoport.Location = new System.Drawing.Point(78, 71);
+            this.cmb_arduinoport.Name = "cmb_arduinoport";
+            this.cmb_arduinoport.Size = new System.Drawing.Size(248, 21);
+            this.cmb_arduinoport.TabIndex = 10;
             // 
             // panel1
             // 
             this.panel1.Controls.Add(this.label1);
             this.panel1.Controls.Add(this.lbl_login);
-            this.panel1.Controls.Add(this.textBox2);
-            this.panel1.Controls.Add(this.textBox1);
+            this.panel1.Controls.Add(this.tbx_divisor);
+            this.panel1.Controls.Add(this.tbx_grupscicle);
             this.panel1.Controls.Add(this.btn_configurar);
             this.panel1.Location = new System.Drawing.Point(66, 129);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(419, 197);
+            this.panel1.Size = new System.Drawing.Size(429, 197);
             this.panel1.TabIndex = 11;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(67)))), ((int)(((byte)(111)))), ((int)(((byte)(138)))));
+            this.label1.Location = new System.Drawing.Point(69, 97);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(85, 25);
+            this.label1.TabIndex = 15;
+            this.label1.Text = "Divisor";
+            // 
+            // lbl_login
+            // 
+            this.lbl_login.AutoSize = true;
+            this.lbl_login.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_login.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(67)))), ((int)(((byte)(111)))), ((int)(((byte)(138)))));
+            this.lbl_login.Location = new System.Drawing.Point(19, 35);
+            this.lbl_login.Name = "lbl_login";
+            this.lbl_login.Size = new System.Drawing.Size(135, 25);
+            this.lbl_login.TabIndex = 14;
+            this.lbl_login.Text = "Grups Cicle";
+            // 
+            // tbx_divisor
+            // 
+            this.tbx_divisor.Location = new System.Drawing.Point(160, 97);
+            this.tbx_divisor.Multiline = true;
+            this.tbx_divisor.Name = "tbx_divisor";
+            this.tbx_divisor.Size = new System.Drawing.Size(88, 36);
+            this.tbx_divisor.TabIndex = 13;
+            // 
+            // tbx_grupscicle
+            // 
+            this.tbx_grupscicle.Location = new System.Drawing.Point(160, 30);
+            this.tbx_grupscicle.Multiline = true;
+            this.tbx_grupscicle.Name = "tbx_grupscicle";
+            this.tbx_grupscicle.Size = new System.Drawing.Size(88, 36);
+            this.tbx_grupscicle.TabIndex = 12;
             // 
             // btn_configurar
             // 
@@ -93,44 +131,6 @@ namespace App
             this.btn_configurar.UseVisualStyleBackColor = false;
             this.btn_configurar.Click += new System.EventHandler(this.btn_configurar_Click);
             // 
-            // textBox1
-            // 
-            this.textBox1.Location = new System.Drawing.Point(160, 30);
-            this.textBox1.Multiline = true;
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(88, 36);
-            this.textBox1.TabIndex = 12;
-            // 
-            // textBox2
-            // 
-            this.textBox2.Location = new System.Drawing.Point(160, 97);
-            this.textBox2.Multiline = true;
-            this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(88, 36);
-            this.textBox2.TabIndex = 13;
-            // 
-            // lbl_login
-            // 
-            this.lbl_login.AutoSize = true;
-            this.lbl_login.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lbl_login.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(67)))), ((int)(((byte)(111)))), ((int)(((byte)(138)))));
-            this.lbl_login.Location = new System.Drawing.Point(19, 35);
-            this.lbl_login.Name = "lbl_login";
-            this.lbl_login.Size = new System.Drawing.Size(135, 25);
-            this.lbl_login.TabIndex = 14;
-            this.lbl_login.Text = "Grups Cicle";
-            // 
-            // label1
-            // 
-            this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(67)))), ((int)(((byte)(111)))), ((int)(((byte)(138)))));
-            this.label1.Location = new System.Drawing.Point(69, 97);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(85, 25);
-            this.label1.TabIndex = 15;
-            this.label1.Text = "Divisor";
-            // 
             // frm_balisses
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
@@ -138,7 +138,7 @@ namespace App
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(247)))), ((int)(((byte)(251)))), ((int)(((byte)(225)))));
             this.ClientSize = new System.Drawing.Size(563, 351);
             this.Controls.Add(this.panel1);
-            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.cmb_arduinoport);
             this.Controls.Add(this.btn_connectar);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.Name = "frm_balisses";
@@ -152,10 +152,10 @@ namespace App
         #endregion
 
         private System.Windows.Forms.Button btn_connectar;
-        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.ComboBox cmb_arduinoport;
         private System.Windows.Forms.Panel panel1;
-        private System.Windows.Forms.TextBox textBox2;
-        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox tbx_divisor;
+        private System.Windows.Forms.TextBox tbx_grupscicle;
         private System.Windows.Forms.Button btn_configurar;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label lbl_login;
--- MESSI_DUALproject/frm_balisses.cs
+++ MESSI_DUALproject/frm_balisses.cs
@@ -12,9 +12,17 @@ namespace App
 {
     public partial class frm_balisses : Form
     {
+        System.IO.Ports.SerialPort ArduinoPort;
+
         public frm_balisses()
         {
             InitializeComponent();
+
+            //crear Serial Port
+            ArduinoPort = new System.IO.Ports.SerialPort();
+            ArduinoPort.PortName = "COM4";  //sustituir por vuestro 
+            ArduinoPort.BaudRate = 9600;
+            ArduinoPort.Open();
         }
 
         private void btn_connectar_Click(object sender, EventArgs e)
